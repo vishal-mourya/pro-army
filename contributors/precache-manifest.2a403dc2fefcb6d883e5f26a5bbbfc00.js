3f482e01e8ce02046423a563e7654834c422dbe5
vishal-mourya <52701972+vishal-mourya@users.noreply.github.com>
2021-09-01 18:13:09 +0530

    added contributors page

---
+++ contributors/precache-manifest.2a403dc2fefcb6d883e5f26a5bbbfc00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8681a2eeae88c8059372575008a5d7ce",
+    "url": "/contributors/index.html"
+  },
+  {
+    "revision": "776ce442413587b6d776",
+    "url": "/contributors/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bcfca67796a4337eee38",
+    "url": "/contributors/static/css/main.c44b5fb9.chunk.css"
+  },
+  {
+    "revision": "776ce442413587b6d776",
+    "url": "/contributors/static/js/2.65586bb6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/contributors/static/js/2.65586bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfca67796a4337eee38",
+    "url": "/contributors/static/js/main.92fe67da.chunk.js"
+  },
+  {
+    "revision": "8710aa94da7779ed49cb",
+    "url": "/contributors/static/js/runtime-main.646d72dc.js"
+  }
+]);
